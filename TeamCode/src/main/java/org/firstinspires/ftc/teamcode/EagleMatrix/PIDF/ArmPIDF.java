bdcf305305be5c033380495a94e6c739a52e84b3
jayden <juvbiama@icloud.com>
2025-02-09 13:14:36 -0700

    updated class that operates the shoulder using pidf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EagleMatrix/PIDF/ArmPIDF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EagleMatrix/PIDF/ArmPIDF.java
@@ -15,15 +15,40 @@ public class ArmPIDF {
 
     private final double ticksInDegree = 700 / 180.0;
 
+    private final int  scoreTarget = 3000;
+    private final int initTarget = 0;
 
+     private DcMotor shoulder;
 
-    DcMotor shoulder;
-
-    public void ArmPID(HardwareMap hw){
+    public ArmPIDF(HardwareMap hw) {
         shoulder = hw.get(DcMotor.class,"shoulder");
         shoulder.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
+
+
+    public void MoveToInit(){
+        int armPosition = shoulder.getCurrentPosition();
+
+        double pid = controller.calculate(armPosition,initTarget);
+        double ff = Math.cos(Math.toRadians(initTarget / ticksInDegree)) * f;
+
+        double power = pid + ff;
+
+        shoulder.setPower(power);
+    }
+
+    public void MoveToScore(){
+        int armPosition = shoulder.getCurrentPosition();
+
+        double pid = controller.calculate(armPosition,scoreTarget);
+        double ff = Math.cos(Math.toRadians(scoreTarget / ticksInDegree)) * f;
+
+        double power = pid + ff;
+
+        shoulder.setPower(power);
+
+    }
     public void MoveToTarget(int target){
         controller.setPID(p,i,d);
 
