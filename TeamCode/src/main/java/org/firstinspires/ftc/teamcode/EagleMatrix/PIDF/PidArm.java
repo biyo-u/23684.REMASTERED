977e9064a599d21e39035eeab68d63ec9170a6e7
jayden <juvbiama@icloud.com>
2025-02-08 19:15:00 -0700

    stared making pids for arm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EagleMatrix/PIDF/PidArm.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.EagleMatrix.PIDF;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Config
+@TeleOp
+public class PidArm  extends OpMode {
+
+    private PIDFController controller;
+
+    public static double p = 0 , i = 0 , d = 0;
+    public static double f = 0;
+    public static int target = 0;
+    public final double ticksInDegree = 700 / 180.0;
+    DcMotor shoulder;
+
+    @Override
+    public void init(){
+
+        controller = new PIDFController(p,i,d,0);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        shoulder = hardwareMap.get(DcMotor.class,"shoulder");
+
+
+
+  }
+    @Override
+    public void loop(){
+
+        controller.setPIDF(p,i,d,0);
+
+        int armPosition = shoulder.getCurrentPosition();
+
+        double pid = controller.calculate(armPosition,target);
+        double ff = Math.cos(Math.toRadians(target / ticksInDegree)) * f;
+
+        double power = pid + ff;
+
+        shoulder.setPower(power);
+
+        telemetry.addData("pos ",armPosition);
+        telemetry.addData("target ",target);
+        telemetry.update();
+
+    }
+}
