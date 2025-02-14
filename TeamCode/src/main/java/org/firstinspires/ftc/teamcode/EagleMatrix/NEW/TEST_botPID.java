a072dc769e69150298d3c58a866c52e58a5252ad
biyo-u <biyouvbiama@gmail.com>
2025-02-13 23:38:26 -0700

    Update TEST_botPID OpMode
    -   Removed unused `DcMotor`, `GoBildaPinpointDriver`, and field declarations.
    - Added `@Autonomous` annotation and assigned it to the `Testing` group for better organization.
    - Removed unused hardware assignments
    - Removed unused hardware initialization code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EagleMatrix/NEW/TEST_botPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EagleMatrix/NEW/TEST_botPID.java
@@ -2,36 +2,24 @@ package org.firstinspires.ftc.teamcode.EagleMatrix.NEW;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
-import org.firstinspires.ftc.teamcode.Subsystems.GoBildaPinpointDriver;
+import org.firstinspires.ftc.teamcode.Utilities.Constants;
 import org.firstinspires.ftc.teamcode.Utilities.Robot;
 
 /** @noinspection unused*/
+@Autonomous(name = "TEST_botPID", group = Constants.GroupNames.Testing)
 public class TEST_botPID extends OpMode {
     // SOFTWARE
     botPID bot;
 
     // HARDWARE
     Robot robot;
-    DcMotor liftMotorLeft;
-    DcMotor liftMotorRight;
-    DcMotor shoulder;
-    GoBildaPinpointDriver odo;
 
     @Override
     public void init() {
         robot = new Robot(hardwareMap,telemetry);
-        liftMotorLeft = hardwareMap.get(DcMotor.class, "liftMotorLeft");
-        liftMotorRight = hardwareMap.get(DcMotor.class, "liftMotorRight");
-        shoulder = hardwareMap.get(DcMotor.class, "shoulder");
-        odo = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
-        liftMotorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        shoulder.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        bot = new botPID(robot);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         bot.setArmTarget(700);
         bot.setLiftTarget(0);
