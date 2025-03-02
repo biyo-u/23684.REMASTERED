3e37f485a33ad0b1b0bd0784648a3c7ba8466366
Aarav Sharma <iamcoder18@gmail.com>
2025-03-01 19:07:01 -0700

    feat: converted AutoTestNew to CommandOpMode
    - Updated AutoTestNew to extend CommandOpMode instead of OpMode
    - Removed unnecessary code, such as telemetry and Alliance enums
    - Replaced `init()`, `start()`, `loop()`, and `stop()` with `initialize()` and `schedule()`
    - Removed doNothing command
    - Scheduled justmovealready to run on `initialize()`
    
    Signed-off-by: Aarav Sharma <iamcoder18@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/AUTO/AutoTestNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/AUTO/AutoTestNew.java
@@ -1,89 +1,13 @@
 package org.firstinspires.ftc.teamcode.OpModes.AUTO;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.arcrobotics.ftclib.command.Command;
-import com.arcrobotics.ftclib.command.CommandBase;
-import com.arcrobotics.ftclib.command.CommandScheduler;
-import com.arcrobotics.ftclib.command.ParallelCommandGroup;
-import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.VoltageSensor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
-import org.firstinspires.ftc.teamcode.Subsystems.Drive;
-import org.firstinspires.ftc.teamcode.OpModes.AUTO.justmovealready;
 
 @Autonomous(name="AutoTest New!")
-public class AutoTestNew extends OpMode {
-
-    Drive drive;
-    VoltageSensor battery;
-    ElapsedTime runtime = new ElapsedTime();
-    justmovealready justmovealready;
-
-    /** @noinspection unused*/
-
-    public enum Alliance {RED, BLUE};
-
-    public AutoTestNew.Alliance getAlliance() {
-        return AutoTestNew.Alliance.RED;
-    }
+public class AutoTestNew extends CommandOpMode {
 
     @Override
-    public void init() {
-        CommandScheduler.getInstance().reset();
-
-        drive = new Drive(hardwareMap);
-        battery = hardwareMap.voltageSensor.get("Control Hub");
-
-        CommandScheduler.getInstance().registerSubsystem(drive);
-
-        drive.reset();
-    }
-
-    public Command doNothing(long timeout) {
-        return new CommandBase() {}.withTimeout(timeout);
-    }
-
-    @Override
-    public void start() {
-        // set starting position
-
-        drive.setPosition(new Pose2D(DistanceUnit.INCH, 0.43, -1.61, AngleUnit.DEGREES, 0));
-        runtime.reset();
-
-        CommandScheduler.getInstance().schedule(
-                justmovealready.withTimeout(3000)
-
-//        new SequentialCommandGroup(
-//                drive.moveQuickly(0, 24),
-//                drive.moveQuickly(0, 0)
-//        )
-        );
-    }
-
-    @Override
-    public void loop() {
-        drive.readSensors();
-        CommandScheduler.getInstance().run();
-
-        TelemetryPacket pack = new TelemetryPacket();
-        pack.put("Elapsed time", runtime.toString());
-        pack.put("time", time);
-        pack.put("battery", battery.getVoltage());
-        drive.addTelemetry(pack);
-        FtcDashboard.getInstance().sendTelemetryPacket(pack);
-        // note, seems that "drawing stuff" commands have to go in their own packet
-    }
-
-    @Override
-    public void stop() {
-        drive.readSensors();
-        drive.stop();
+    public void initialize() {
+        schedule(new justmovealready());
     }
 }
